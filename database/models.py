ef862291f401fba92eff7aeb7266f5edeed9dbf3
ivand1988 <ivand1988@outlook.com>
2022-05-16 19:57:43 +0300

    Added delete api + tests

--- database/models.py
+++ database/models.py
@@ -11,26 +11,43 @@ association_table = Table(
 )
 
 
+# class Association(Base):
+#     __tablename__ = "association"
+# 
+#     authors_id = Column(ForeignKey("authors.id"), primary_key=True)
+#     books_id = Column(ForeignKey("books.id"), primary_key=True)
+# 
+#     author = relationship("Author", back_populates="books")
+#     book = relationship("Book", back_populates="authors")
+
+
 class Author(Base):
     __tablename__ = "authors"
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(100), nullable=False, unique=True)
 
-    books = relationship("Book", secondary=association_table, back_populates="authors")
+    books = relationship("Book", secondary=association_table, back_populates="authors", passive_deletes=False, cascade="save-update" )
 
     def __str__(self) -> str:
         return f"{self.name}"
+
+    def __repr__(self) -> str:
+        return self.name
 
 
 class Book(Base):
     __tablename__ = "books"
 
     id = Column(Integer, primary_key=True, index=True)
-    title = Column(String(100), nullable=False)
+    number = Column(Integer, nullable=False)
+    title = Column(String(100), nullable=False, index=True)
     description = Column(String, nullable=True)
 
-    authors = relationship("Author", secondary=association_table, back_populates="books")
+    authors = relationship("Author", secondary=association_table, back_populates="books", passive_deletes=False, cascade="save-update" )
 
     def __str__(self) -> str:
         return f"{self.title}, {self.authors}"
+
+    def __repr__(self) -> str:
+        return self.title
